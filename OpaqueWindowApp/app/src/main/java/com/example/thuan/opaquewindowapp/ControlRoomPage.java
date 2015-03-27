9872f6071a4bb5a7b6a7d6032f5a237a8648f554
tiffanytle <tiffanyisle@gmail.com>
2015-03-26 21:21:58 -0700

    Attempting to connect to network

--- OpaqueWindowApp/app/src/main/java/com/example/thuan/opaquewindowapp/ControlRoomPage.java
+++ OpaqueWindowApp/app/src/main/java/com/example/thuan/opaquewindowapp/ControlRoomPage.java
@@ -1,15 +1,18 @@
 package com.example.thuan.opaquewindowapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
-import android.support.v7.app.ActionBarActivity;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class ControlRoomPage extends Activity implements View.OnClickListener {
@@ -17,6 +20,8 @@ public class ControlRoomPage extends Activity implements View.OnClickListener {
     private Button winButton1, winButton2, winButton3, winButton4;
     ColorDrawable buttonColor;
     int colorID;
+
+    private Context context;
 
     boolean togglePane1 = false;
     boolean togglePane2 = false;
@@ -37,6 +42,16 @@ public class ControlRoomPage extends Activity implements View.OnClickListener {
         winButton2.setOnClickListener(this);
         winButton3.setOnClickListener(this);
         winButton4.setOnClickListener(this);
+
+        ConnectivityManager check = (ConnectivityManager) this.context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] info = check.getAllNetworkInfo();
+
+        for (int i = 0; i<info.length; i++){
+            if (info[i].getState() == NetworkInfo.State.CONNECTED){
+                Toast.makeText(context, "Internet is connected",Toast.LENGTH_SHORT).show();
+            }
+        }
+
     }
 
     @Override
