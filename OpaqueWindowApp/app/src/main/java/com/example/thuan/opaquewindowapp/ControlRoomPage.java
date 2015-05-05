39737c2c15c96f3fb7faf8afb6cd7e6db4ce60c2
Thuan Chu <thuanchoo@gmail.com>
2015-05-04 21:26:51 -0700

    Working code commit
    Working code for Project Demo
    Comments required, additional error checks should be added. (Not
    required for demonstration)

--- OpaqueWindowApp/app/src/main/java/com/example/thuan/opaquewindowapp/ControlRoomPage.java
+++ OpaqueWindowApp/app/src/main/java/com/example/thuan/opaquewindowapp/ControlRoomPage.java
@@ -1,8 +1,10 @@
 package com.example.thuan.opaquewindowapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
@@ -11,35 +13,36 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.widget.Button;
 import android.widget.Toast;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+public class ControlRoomPage extends Activity {
 
-public class ControlRoomPage extends Activity implements View.OnClickListener {
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
 
-    private Button winButton1, winButton2, winButton3, winButton4;
-    ColorDrawable buttonColor;
-    int colorID;
+    //allow back button to bo back to select room page
+    @Override
+    public void onBackPressed()
+    {
+        super.onBackPressed();
+        startActivity(new Intent(ControlRoomPage.this, SelectRoomPage.class));
+        finish();
 
-    boolean togglePane1 = false;
-    boolean togglePane2 = false;
-    boolean togglePane3 = false;
-    boolean togglePane4 = false;
-
+    }
+    WebView webview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_room_page);
-
-        winButton1 = (Button) findViewById(R.id.winButton1);
-        winButton2 = (Button) findViewById(R.id.winButton2);
-        winButton3 = (Button) findViewById(R.id.winButton3);
-        winButton4 = (Button) findViewById(R.id.winButton4);
-
-        winButton1.setOnClickListener(this);
-        winButton2.setOnClickListener(this);
-        winButton3.setOnClickListener(this);
-        winButton4.setOnClickListener(this);
 
         ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo data = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
@@ -49,41 +52,50 @@ public class ControlRoomPage extends Activity implements View.OnClickListener {
             Toast.makeText(getApplicationContext(), "Internet is connected",Toast.LENGTH_SHORT).show();
 
         }
+
         else if (cm.getNetworkInfo(0).getState() == NetworkInfo.State.DISCONNECTED ||
                 cm.getNetworkInfo(1).getState() == NetworkInfo.State.DISCONNECTING) {
             Toast.makeText(getApplicationContext(), "Internet is not connected",Toast.LENGTH_SHORT).show();
 
         }
+        webview=(WebView)findViewById(R.id.webView);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebViewClient(new MyWebViewClient());
+
+        final Context myApp = this;
+
+/* Set web client to allow javascript messages */
+        webview.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, final android.webkit.JsResult result)
+            {
+                new AlertDialog.Builder(myApp)
+                        .setTitle("          System Message")
+                        .setMessage(message)
+                        .setPositiveButton(android.R.string.ok,
+                                new AlertDialog.OnClickListener()
+                                {
+                                    public void onClick(DialogInterface dialog, int which)
+                                    {
+                                        result.confirm();
+                                    }
+                                })
+                        .setCancelable(false)
+                        .create()
+                        .show();
+
+                return true;
+            };
+        });
+        openURL();
+    }
+    /** Opens the URL in a browser */
+    private void openURL() {
+
+        webview.loadUrl("http://192.168.43.17");
+        webview.requestFocus();
     }
 
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()) {
-            case R.id.winButton1:
-                togglePane1 = changeWindow(v,togglePane1);
-                break;
-            case R.id.winButton2:
-                togglePane2 = changeWindow(v,togglePane2);
-                break;
-            case R.id.winButton3:
-                togglePane3 = changeWindow(v,togglePane3);
-                break;
-            case R.id.winButton4:
-                togglePane4 = changeWindow(v,togglePane4);
-                break;
-            default:
-                break;
-        }
-    }
 
-    public boolean changeWindow(View v, boolean toggle) {
-
-        if (toggle)
-            v.setBackgroundResource(R.drawable.windowbutton);
-        else
-            v.setBackgroundResource(R.drawable.opaquewindow);
-        return (!toggle);
-
-    }
 
 }
--- OpaqueWindowApp/app/src/main/java/com/example/thuan/opaquewindowapp/SelectRoomPage.java
+++ OpaqueWindowApp/app/src/main/java/com/example/thuan/opaquewindowapp/SelectRoomPage.java
@@ -19,6 +19,8 @@ public class SelectRoomPage extends Activity implements View.OnClickListener {
 
     boolean roomExists = false;
     final Context context = this;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
